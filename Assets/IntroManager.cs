989ca886def2dae171a664a926e3552d5744cc5f
ubirehman <rehmanubaid5@gmail.com>
2023-06-27 21:10:46 +0500

    Updatng packages.

--- Assets/IntroManager.cs
+++ Assets/IntroManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class IntroManager : MonoBehaviour
 {
+    [SerializeField] Image loadingBarFillerImage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        loadingBarFillerImage.DOFillAmount(1, 2f).SetEase(Ease.Linear).OnComplete(()=> SceneManager.LoadScene(1));
     }
 
     // Update is called once per frame
